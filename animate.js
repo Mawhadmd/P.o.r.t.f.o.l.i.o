949282566fe2a2837aaf37fa0be0646f5cd3b683
Mawhadmd <104397399+Mawhadmd@users.noreply.github.com>
2024-07-29 10:35:58 +0400

    Updated

--- animate.js
+++ animate.js
@@ -1,7 +1,14 @@
 var elemetns = document.querySelectorAll('.animate')
 console.log(elemetns)
-var whatever = IntersectionObserver(()=>{
-    elemetns.forEach((e)=>{
-        
+var observer = new IntersectionObserver((elemetng)=>{
+    elemetng.forEach((e)=>{
+        if(e.isIntersecting){
+            e.target.classList.add('visible')
+        }
+        else{
+            e.target.classList.remove('visible')
+        }
     })
-},{})
+},{threshold:0.15})
+
+elemetns.forEach((e)=>{observer.observe(e)})
